f4b01b00cb53f672d5008778f4f79722684a0cfa
Behzad-Az <behy1368@gmail.com>
2020-09-26 22:51:22 +0000

    added without function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,4 +15,4 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3], true);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
---
+++ without.js
@@ -0,0 +1,28 @@
+const compareArrElmements = function(arr1, arr2) {
+  let equalValCount = 0;
+  arr1.forEach((elm, indx) => {
+    if (elm === arr2[indx]) {
+      equalValCount += 1;
+    }
+  });
+  return equalValCount;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  actual.length === expected.length && actual.length === compareArrElmements(actual, expected) ?
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const without = function(sourceArr, withoutArr) {
+  let outputArr = [];
+  sourceArr.forEach(element => {
+    if (!withoutArr.includes(element)) {
+      outputArr.push(element);
+    }
+  });
+  return outputArr;
+};
+
+// TEST CODE
+assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
